8c6dc86331ddb24a32bfaf01075be0fcdad5deec
Abigail Burtley <abigailtop95@yahoo.com>
2023-09-08 19:24:52 -0400

    add constructor blue print file to hold the blue print of shapes, created seperate shape files to create new instance of blue print

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateLogo = require('.gener')
+const circle = require('./shapes/circle');
+const triangle = require('./shapes/triangle');
+const square = require('./shapes/square');
+const constructor = require('./shapes/constructor');
