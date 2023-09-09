9a04684e70f823cd206d89471a0ff87adaa903a4
Abigail Burtley <abigailtop95@yahoo.com>
2023-09-08 20:05:06 -0400

    add prompt questions to index js file, these will take user input and generate the shape, color, text color

--- index.js
+++ index.js
@@ -4,3 +4,44 @@ const circle = require('./shapes/circle');
 const triangle = require('./shapes/triangle');
 const square = require('./shapes/square');
 const constructor = require('./shapes/constructor');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters.',
+        validate: (text) => {
+            if(text === "") {
+                return 'Please enter a valid number of characters to create LOGO.'
+            }
+            return true
+        }
+
+    },
+    {
+        type: 'choice',
+        name: 'color',
+        message: 'Choose a shape for the LOGO:',
+        choices: ['Triangle', 'Circle', 'Square'],
+        validate: (choices) => {
+            if(choices === "") {
+                return 'Shape is required to create LOGO.'
+            }
+            return true
+        }
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter a text color or hexadecimal number (#number).',
+        validate: (input) => {
+            if(input === "") {
+                return 'Invalid input, Enter a text color or hexadecimal number starting with a number symbol.'
+            }
+            return true
+        }
+    }
+
+
+
+]
--- shapes/circle.js
+++ shapes/circle.js
@@ -3,11 +3,16 @@ const Shape = require('./constructor')
 class Circle extends Shape {
 render() {
 return
-`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+`
+Your Logo:
+
+<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
   <circle cx="150" cy="100" r="80" fill="${this.color}" />
 
-  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text></svg>`
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
+  
+  </svg>`
 
 }
 }
