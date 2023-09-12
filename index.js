d6ac6ddfe7bfb04550d02ae285334bc06e6ee55f
Abigail Burtley <abigailtop95@yahoo.com>
2023-09-12 02:13:48 -0400

    add message to user for when file is created with customized logo

--- index.js
+++ index.js
@@ -5,99 +5,104 @@ const Triangle = require('./shapes/triangle');
 const Square = require('./shapes/square');
 
 inquirer
-.prompt([
-    {
-        type: 'input',
-        name: 'text',
-        message: 'Enter up to three characters.',
-        validate: (text) => {
-            if(text === "") {
-                return 'Please enter a valid number of characters to create LOGO.'
+    .prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter up to three characters.',
+            validate: (text) => {
+                if (text === "") {
+                    return 'Please enter a valid number of characters to create LOGO.'
+                }
+                return true
             }
-            return true
-        }
 
-    },
-    {
-        type: 'list',
-        name: 'shape',
-        message: 'Choose a shape for the LOGO:',
-        choices: ['Triangle', 'Circle', 'Square'],
-        validate: (choices) => {
-            if(choices === "") {
-                return 'Shape is required to create LOGO.'
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Choose a shape for the LOGO:',
+            choices: ['Triangle', 'Circle', 'Square'],
+            validate: (choices) => {
+                if (choices === "") {
+                    return 'Shape is required to create LOGO.'
+                }
+                return true
             }
-            return true
-        }
-    },
-    {
-        type: 'input',
-        name: 'color',
-        message: 'Enter a shape color or hexadecimal number.',
-        validate: (input) => {
-            if(input === "") {
-                return 'No color has been provided. Enter a color for the LOGO shape.'
+        },
+        {
+            type: 'input',
+            name: 'color',
+            message: 'Enter a shape color or hexadecimal number.',
+            validate: (input) => {
+                if (input === "") {
+                    return 'No color has been provided. Enter a color for the LOGO shape.'
+                }
+                return true
             }
-            return true
-        }
 
-    },
-    {
-        type: 'input',
-        name: 'textColor',
-        message: 'Enter a text color or hexadecimal number (#number).',
-        validate: (input) => {
-            if(input === "") {
-                return 'Invalid input, Enter a text color or hexadecimal number starting with a number symbol.'
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter a text color or hexadecimal number (#number).',
+            validate: (input) => {
+                if (input === "") {
+                    return 'Invalid input, Enter a text color or hexadecimal number starting with a number symbol.'
+                }
+                return true
             }
-            return true
+        },
+        {
+            type: 'input',
+            name: 'fontSize',
+            message: 'Enter a font size. If no font size entered, a default font size of 60 will be set.',
+            initial: '60'
         }
-    },
-    {
-        type: 'input',
-        name: 'fontSize',
-        message: 'Enter a font size. If no font size entered, a default font size of 60 will be set.',
-        initial: '60'
-    }
-]).then((data) => {
-    const fontSize = data.fontSize || 60;
-    console.log(`Shape: ${data.shape}`)
-    console.log(`Color: ${data.color}`);
-    console.log(`Text: ${data.text}`);
-    console.log(`Text Color: ${data.textColor}`)
-    console.log(`Selected font size: ${fontSize}`);
-    console.log(`CREATING LOGO....`);
+    ]).then((data) => {
+        const fontSize = data.fontSize || 60;
+        console.log(`Shape: ${data.shape}`)
+        console.log(`Color: ${data.color}`);
+        console.log(`Text: ${data.text}`);
+        console.log(`Text Color: ${data.textColor}`)
+        console.log(`Selected font size: ${fontSize}`);
+        console.log(`CREATING LOGO....`);
 
 
-let shape;
+        let shape;
 
-switch(data.shape.toLowerCase()) {
-    case 'circle':
-        shape = new Circle(data.color, data.text, data.textColor, data.fontSize)
-        break;
-    case 'triangle':
-        shape = new Triangle(data.color, data.text, data.textColor, data.fontSize);
-        break;
-    case 'square':
-        shape = new Square(data.color, data.text, data.textColor, data.fontSize);
-        break;
-    default:
-        console.log("Error: Invalid shape choice.");
-        return;
-}
-    const svgLogo = shape.render();
-    setTimeout(() => {
-    fs.writeFile(`${data.shape.toLowerCase()}.svg`, svgLogo, (err) => {
-        if(err) {
-            console.error(`Error: ${err}`);
-        } else {
-    console.log(`File ${data.shape.toLowerCase()}.svg has been created with the logo.`)
-    };
+        switch (data.shape.toLowerCase()) {
+            case 'circle':
+                shape = new Circle(data.color, data.text, data.textColor, data.fontSize)
+                break;
+            case 'triangle':
+                shape = new Triangle(data.color, data.text, data.textColor, data.fontSize);
+                break;
+            case 'square':
+                shape = new Square(data.color, data.text, data.textColor, data.fontSize);
+                break;
+            default:
+                console.log("Error: Invalid shape choice.");
+                return;
+        }
+        const svgLogo = shape.render();
+
+        setTimeout(() => {
+
+
+            fs.writeFile(`${data.shape.toLowerCase()}.svg`, svgLogo, (err) => {
+                if (err) {
+                    console.error(`Error: ${err}`);
+                } else {
+
+                    console.log(`File ${data.shape.toLowerCase()}.svg has been created with the logo.`)
+                };
+
+            });
+
+        }, "5000");
 
     });
-
-    }, "5000");
-});
 
 
 
